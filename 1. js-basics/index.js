4c7884e81d5ab1901140a2cc640f5df1ec0caeab
HeySkidee <harshitojha46@gmail.com>
2022-02-25 20:58:53 +0530

    js, es6 basics from programming with mosh

---
+++ 1. js-basics/index.js
@@ -0,0 +1,6 @@
+import Teacher from './teacher.js'; 
+
+
+const teacher =  new Teacher('vandana mam', 'MSc');
+teacher.teach();
+console.log(teacher.degree);
---
+++ 1. js-basics/modules representation.js
@@ -0,0 +1,10 @@
+// import { Teacher } from './teacher.js';
+// import { Person } from './person.js';
+
+// const skida = new Person('skidee');
+
+// skida.name();
+// console.log(skida);
+
+// const teacher =  new Teacher('vandana mam', 'MSc');
+// teacher.teach();
