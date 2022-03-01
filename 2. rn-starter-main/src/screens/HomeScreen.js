ec83cb7588fbb0a0c73558a61083a200c89c5d04
HeySkidee <harshitojha46@gmail.com>
2022-03-01 20:35:43 +0530

    add navigation buttons

--- 2. rn-starter-main/src/screens/HomeScreen.js
+++ 2. rn-starter-main/src/screens/HomeScreen.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
   return <View>
   <Text style={styles.text}>lowsensi</Text>
   <Button 
-    onPress={() => console.log("Button Pressed")}
+    onPress={() => navigation.navigate('Components')}
     title="Got to Components Demo"
   />
-  <TouchableOpacity onPress={() => console.log("List Pressed")}>
+  <Button
+    title='Go to List Demo'
+    onPress={() => navigation.navigate('List')}
+  />
+  {/* <TouchableOpacity onPress={() => props.navigation.navigate('List')}>
     <Text>Go to List Demo</Text>
-  </TouchableOpacity>
+    <Text>Go to List Demo</Text>
+    <Text>Go to List Demo</Text>
+  </TouchableOpacity> */}
   </View>
 };
 
